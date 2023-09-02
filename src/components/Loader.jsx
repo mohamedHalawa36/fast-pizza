670286d518b95042af46d818190db06fe090fd7e
mohamedHalawa36 <mohamedhalawah22@gmail.com>
2023-09-02 04:22:05 +0300

    feat: add new loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,14 +1,12 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function Loader() {
   return (
-    <div className=" flex items-center justify-center h-screen absolute left-0 top-0 z-30 backdrop-blur-3xl w-screen opacity-50 bg-stone-800">
-      <FontAwesomeIcon
-        className=" text-[70px]"
-        color="rgb(234 179 8)"
-        icon="fa-solid fa-spinner"
-        spin
-        size="xl"
-      />{" "}
+    <div className=" flex items-center justify-center h-screen absolute left-0 top-0 z-30 backdrop-blur-md w-screen bg-opacity-50 bg-stone-800">
+      <span className=" bg-transparent w-[120px] h-[120px]   absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%]">
+        <div className="w-full h-full custom-spin rounded-full border-yellow-500"></div>
+      </span>
+      <span className="text-[50px] rotate-[20deg] absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%]">
+        🍕
+      </span>
     </div>
   );
 }
